6c24ce59dc86bbff74ee95bd7807d09f8478dbe8
youngsunkr <youngsunkr@gmail.com>
2018-09-14 03:06:25 +0900

    Pub/Sub Design Pattern
    https://www.youtube.com/watch?v=frGy-nGoGUY

---
+++ Test/Pub-Sub-DesignPattern/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pub_Sub_DesignPattern
+{
+    /// <summary>
+    /// https://www.youtube.com/watch?v=frGy-nGoGUY
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Publisher dogPublisher = new Publisher();
+            Publisher catPublisher = new Publisher();
+
+            Subscriber AnimalLover = new Subscriber();
+            Subscriber OldCatLady = new Subscriber();
+
+            PubSubServer Server = new PubSubServer();
+
+            Message dogMessage = new Message();
+            dogMessage.topic = "Dogs";
+            dogMessage.payload = "Dogs are man's best friend";
+
+            Message catMessage = new Message();
+            catMessage.topic = "Cat";
+            catMessage.payload = "Cats can take care of themservice";
+
+            dogPublisher.Send(dogMessage, Server);
+            catPublisher.Send(catMessage, Server);
+
+            AnimalLover.Listen("Dogs", 0);
+            AnimalLover.Listen("Cats", 1);
+
+            OldCatLady.Listen("Cats", 0);
+
+            Server.subscribers[0] = AnimalLover;
+            Server.subscribers[1] = OldCatLady;
+
+            Server.Forword();
+
+            Console.WriteLine("AnimalLover has subscribed to the forwording message");
+            AnimalLover.Print();
+
+            Console.WriteLine("");
+
+            Console.WriteLine("OldCatLady has subscribed to the forwording message");
+            OldCatLady.Print();
+        }
+    }
+}
---
+++ Test/Pub-Sub-DesignPattern/PubSubHeader.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pub_Sub_DesignPattern
+{
+    class Publisher
+    {
+        public Publisher()
+        {
+        }
+
+        public void Send(Message newMessage, PubSubServer myServer)
+        {
+            myServer.buffer.Enqueue(newMessage);
+        }
+    }
+
+    
+    class Subscriber
+    {
+        public Subscriber()
+        {
+        }
+
+        public string[] topics = new string[2];
+        public Queue<Message> myMessages = new Queue<Message>();
+        public void Listen(string newTopic, int index)
+        {
+            topics[index] = newTopic;
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < topics.Length; i++)
+            {
+                Message newMessage = myMessages.Dequeue();
+                Console.WriteLine("Topics: " + newMessage.topic + "\n" + newMessage.payload);
+            }
+        }
+    }
+
+    class Message
+    {
+        public Message()
+        {
+        }
+
+        public string payload;
+        public string topic;
+    }
+
+
+    class PubSubServer
+    {
+        public PubSubServer()
+        {
+        }
+
+        public Queue<Message> buffer = new Queue<Message>();
+        public Subscriber[] subscribers = new Subscriber[2];
+
+        public void Forword()
+        {
+            while (buffer.Count != 0)
+            {
+                Message tempMessage = buffer.Dequeue();
+                for (int i = 0; i < subscribers.Length; i++)
+                {
+                    for (int j = 0; j < subscribers[i].topics.Length; j++)
+                    {
+                        if (tempMessage.topic == subscribers[i].topics[j])
+                        {
+                            subscribers[i].myMessages.Enqueue(tempMessage);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
